adfa562a3dae83e7a22321bd4a114fc4a649a819
Dustin Row <drow@redhat.com>
2026-06-25 15:59:31 -0700

    Fix bundle channel name to #forum-hcm-deployments (#96)

--- pkg/config/ci_status_jobs.go
+++ pkg/config/ci_status_jobs.go
@@ -8,6 +8,8 @@ type CIStatusConfig struct {
 type StatusCategory struct {
 	ID         string      `yaml:"id"`
 	Name       string      `yaml:"name"`
+	Scope      string      `yaml:"scope,omitempty"`
+	Components []string    `yaml:"components,omitempty"`
 	ProwFilter string      `yaml:"prow_filter"`
 	Jobs       []StatusJob `yaml:"jobs"`
 }
